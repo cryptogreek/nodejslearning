010eaf176bbc85b4d42e49c4a949e95888ef4ea5
cryptogreek <cryptogreekcaptions@gmail.com>
2025-08-01 20:34:20 -0700

    start

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+function testButton(buttonId) {
+    console.log("Button clicked:", buttonId);
+    // Additional logic can be added here
+}
+function platformCheckUNITTEST(){
+    const os = require('os');
+    console.log(os.platform());
+}
+
+platformCheckUNITTEST();
